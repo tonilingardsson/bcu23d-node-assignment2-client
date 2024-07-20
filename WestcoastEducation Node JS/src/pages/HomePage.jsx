2d284579527223c577c897751c0729c30636520b
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-20 08:55:41 +0200

    Creating Block component and updating HomePage with it

--- WestcoastEducation Node JS/src/pages/HomePage.jsx
+++ WestcoastEducation Node JS/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Block from '../components/Block';
 import Header from '../components/Header';
 
 function HomePage() {
   return (
     <section className='page'>
       <Header />
+      <Block />
     </section>
   );
 }
