26417591b1c73ef9edd9ef9dd1e517b27afb52a5
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-20 12:30:28 +0200

    Created Global context for the app

--- WestcoastEducation Node JS/src/App.jsx
+++ WestcoastEducation Node JS/src/App.jsx
@@ -1,13 +1,27 @@
-import HomePage from './pages/HomePage';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Router';
+import GlobalContext from './contexts/GlobalContext';
+import { useEffect, useState } from 'react';
+import { validateToken } from './services/userService';
 
 function App() {
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    async function validToken() {
+      const response = await validateToken(localStorage.getItem('TOKEN'));
+      setIsValid(response?.success ? true : false);
+    }
+
+    validToken();
+  }, []);
+
   return (
     <>
-      <RouterProvider router={router} />
+      <GlobalContext.Provider value={{ isValid, setIsValid }}>
+        <RouterProvider router={router} />
+      </GlobalContext.Provider>
     </>
   );
 }
-
 export default App;
