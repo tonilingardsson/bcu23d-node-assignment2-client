b5eabc723f69f86904c3d39b383cc7cf24b90d75
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-24 07:31:11 +0200

    Create an initial homePage

---
+++ lunachain/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Block from '../components/Block';
+
+function HomePage() {
+  return (
+    <section className='page'>
+      <Header />
+      <Block />
+    </section>
+  );
+}
+
+export default HomePage;
