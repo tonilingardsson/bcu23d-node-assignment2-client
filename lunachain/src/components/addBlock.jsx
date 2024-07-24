0d5c22da4ed5b707e38afe15eb2e131c4133cd00
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-24 12:30:12 +0200

    Create addBlock component and an API service

---
+++ lunachain/src/components/addBlock.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { addBlock } from '../services/api';
+
+const AddBlock = () => {
+  const [data, setData] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await addBlock(data);
+    setData('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+        placeholder='Enter block data'
+      />
+      <button type='submit'>Add Block</button>
+    </form>
+  );
+};
+
+export default AddBlock;
