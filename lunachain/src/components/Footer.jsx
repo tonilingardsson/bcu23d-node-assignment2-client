2ce50f1d0546de6098ebff005915457a6e8e969c
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-24 07:49:52 +0200

    Create Footer

---
+++ lunachain/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>Lunchain Copyright 2024. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- lunachain/src/pages/HomePage.jsx
+++ lunachain/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header';
 import Block from '../components/Block';
+import Footer from '../components/Footer';
 
 function HomePage() {
   return (
     <section className='page'>
       <Header />
       <Block />
+      <Footer />
     </section>
   );
 }
