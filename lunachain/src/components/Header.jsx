308034ede7536949cf8a7df503e34416bc865ca7
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-25 11:39:33 +0200

    Create Login component and add it in Header

--- lunachain/src/components/Header.jsx
+++ lunachain/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import logo from '../assets/images/lunachain.jpg';
+import Login from './Login';
 
 function Header() {
   return (
     <header className='header'>
       <h1>Lunachain Block Explorer</h1>
       <img src={logo} className='logo' alt='Lunachain logo' />
+      <div className='login'>
+        <Login />
+      </div>
     </header>
   );
 }
---
+++ lunachain/src/components/Login.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5001/api/v1/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+      if (response.ok) {
+        const result = await response.json();
+        localStorage.setItem('token', result.token);
+      } else {
+        console.log(response.statusText);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Enter email address'
+        />
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='Enter password'
+        />
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
+};
+export default Login;
