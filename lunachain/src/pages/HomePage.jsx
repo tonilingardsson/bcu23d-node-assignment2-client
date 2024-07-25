16f85fbe4fd610df5335ea21f50896a69df8c7d0
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-25 12:22:32 +0200

    Updated AddTransaction name and extension and add it on HomePage

--- lunachain/src/pages/HomePage.jsx
+++ lunachain/src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import Header from '../components/Header';
 import Wallet from '../components/Wallet';
 import Blockchain from '../components/Blockchain';
 import Footer from '../components/Footer';
+import AddTransaction from '../components/AddTransaction.jsx';
 
 function HomePage() {
   return (
     <section className='page'>
       <Header />
       <Wallet />
+      <AddTransaction />
       <Blockchain />
       <Footer />
     </section>
