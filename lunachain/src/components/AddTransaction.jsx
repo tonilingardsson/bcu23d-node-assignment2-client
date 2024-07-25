4ecb81ecf25e796ec1d4fa8acbe5deec71783eec
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-07-25 23:45:55 +0200

    Updated AddTransaction component adding useState for sender, recipient and amount

--- lunachain/src/components/AddTransaction.jsx
+++ lunachain/src/components/AddTransaction.jsx
@@ -1,39 +1,59 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 const AddTransaction = () => {
-    const [sender, setSender] = useState('')
-    const [receiver, setReceiver] = useState('')
-    const [amount, setAmount] = useState('')
+  const [sender, setSender] = useState('');
+  const [recipient, setRecipient] = useState('');
+  const [amount, setAmount] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:5001/api/v1/transactions/transaction', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}` },
-                body: JSON.stringify({ sender, receiver, amount }),
-            });
-            if (response.ok) {
-                const result = await response.json();
-                console.log(result);
-            }
-        } catch (error) {
-            console.log(error);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        'http://localhost:5001/api/v1/transactions/transaction',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+          body: JSON.stringify({ sender, recipient, amount }),
         }
-    };
-    return (
-        <div className="add-transaction">
-            <form onSubmit={handleSubmit}>
-                <label>From:</label>
-                <input type="text" value={sender} onChange={(e) => setSender(e.target.value)} placeholder="Enter sender's address" />
-                <label>To:</label>
-                <input type="text" value={receiver} onChange={(e) => setReceiver(e.target.value)} placeholder="Enter receiver's address" />
-                <label>Amount:</label>
-                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount" />
-                <button type="submit">Add Transaction</button>
-            </form>
-        </div>
-    );
+      );
+      if (response.ok) {
+        const result = await response.json();
+        setSender('');
+        setRecipient('');
+        setAmount('');
+        console.log(result);
+      } else {
+        console.log(response.statusText);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className='add-transaction'>
+      <h2>Add Transaction</h2>
+      <form onSubmit={handleSubmit}>
+        <label>To:</label>
+        <input
+          type='text'
+          value={recipient}
+          onChange={(e) => setRecipient(e.target.value)}
+          placeholder="Enter recipient's address"
+        />
+        <label>Amount:</label>
+        <input
+          type='number'
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder='Enter amount'
+        />
+        <button type='submit'>Add Transaction</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddTransaction;
